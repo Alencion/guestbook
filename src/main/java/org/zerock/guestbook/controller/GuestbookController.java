8444118dab400afff0bd060c2b86b40fa4f34680
Alencion <rhqnrdl625@gmail.com>
2022-01-11 15:27:36 +0900

    [추가] 방명록 조회 페이지 - #9
    - 목록페이지에서 gno 클릭시 해당 조회페이지로 이동

--- src/main/java/org/zerock/guestbook/controller/GuestbookController.java
+++ src/main/java/org/zerock/guestbook/controller/GuestbookController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -33,7 +34,7 @@ public class GuestbookController {
     }
 
     @GetMapping("/register")
-    public void register(){
+    public void register() {
         log.info("register get...");
     }
 
@@ -47,5 +48,15 @@ public class GuestbookController {
         redirectAttributes.addFlashAttribute("msg", gno);
 
         return "redirect:/guestbook/list";
+    }
+
+    @GetMapping("/read")
+    public void read(long gno, @ModelAttribute("requestDTO") PageRequestDTO requestDTO, Model model) {
+
+        log.info("gno: {}", gno);
+
+        GuestbookDTO dto = this.guestbookService.read(gno);
+
+        model.addAttribute("dto", dto);
     }
 }

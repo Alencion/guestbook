f25e034737c2b0dd8412cb92656105f82a865911
Alencion <rhqnrdl625@gmail.com>
2022-01-11 15:53:10 +0900

    [테스트] 방명록 수정 테스트 추가 - #11

--- src/test/java/org/zerock/guestbook/repository/GuestbookRepositoryTests.java
+++ src/test/java/org/zerock/guestbook/repository/GuestbookRepositoryTests.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.zerock.guestbook.entity.Guestbook;
 
+import java.util.Optional;
 import java.util.stream.IntStream;
 
 @SpringBootTest
@@ -21,5 +22,21 @@ public class GuestbookRepositoryTests {
             Guestbook guestbook = Guestbook.builder().title("Title..." + i).content("Content..." + i).writer("user" + (i % 10)).build();
             System.out.println(guestbookRepository.save(guestbook));
         });
+    }
+
+    @Test
+    public void updateTest() {
+        // given
+        Optional<Guestbook> result = this.guestbookRepository.findById(300L);
+
+        // when & then
+        if(result.isPresent()) {
+            Guestbook guestbook = result.get();
+
+            guestbook.changeTitle("Changed Title...");
+            guestbook.changeContent("Changed Content...");
+
+            this.guestbookRepository.save(guestbook);
+        }
     }
 }

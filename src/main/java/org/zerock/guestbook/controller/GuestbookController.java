30bafdd8a6787dd5a3024c5d49bab11b922fa886
Alencion <rhqnrdl625@gmail.com>
2022-01-11 11:24:36 +0900

    [추가] 방명록 목록페이지 구현 - #4

--- src/main/java/org/zerock/guestbook/controller/GuestbookController.java
+++ src/main/java/org/zerock/guestbook/controller/GuestbookController.java
@@ -1,20 +1,30 @@
 package org.zerock.guestbook.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.guestbook.repository.GuestbookRepository;
 
 @Controller
 @Slf4j
 @RequestMapping("/guestbook")
 public class GuestbookController {
 
-    @GetMapping({"/", "/list"})
-    public String list() {
+    @Autowired
+    public GuestbookRepository guestbookRepository;
 
+    @GetMapping("/")
+    public String index() {
+        return "redirect:/guestbook/list";
+    }
+
+    @GetMapping("/list")
+    public void list(Model model) {
         log.info("list...........");
 
-        return "/guestbook/list";
+        model.addAttribute("result", guestbookRepository.findAll());
     }
 }
